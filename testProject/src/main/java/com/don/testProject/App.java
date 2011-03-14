64b6438330bd1fa295c632c8ef4f7f7896ba0dd0
Lydon Chandra <lydonchandra@mini-2.local>
2011-03-15 00:26:50 +0800

    add testProject and maven 3.0.3  binary

---
+++ testProject/src/main/java/com/don/testProject/App.java
@@ -0,0 +1,13 @@
+package com.don.testProject;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
